0e618ef7bf85cdb8b281b3e004e948322e9e2461
Nuwan Chamara Pallewela <nwnpallewela@gmail.com>
2026-03-26 14:12:09 +1100

    Update solution for adam-optimizer

--- adam-optimizer/adam-optimizer.py
+++ adam-optimizer/adam-optimizer.py
@@ -12,22 +12,8 @@ def adam_step(param, grad, m, v, t, lr=1e-3, beta1=0.9, beta2=0.999, eps=1e-8):
     # Write code here
     mt = beta1 * m_d + (1-beta1) * grad_d
     vt = beta2 * v_d + (1-beta2) * (grad_d ** 2)
-    # vt = np.sqrt(v_d + eps)
     m_new = mt / (1-beta1**t)
     v_new = vt / (1-beta2**t)
     param_new = param_d - lr * (m_new / (np.sqrt(v_new) + eps))
 
-    # m_new = beta1 * m + (1 - beta1) * grad
-
-    # Step 2: Update biased second raw moment estimate
-    # v_new = beta2 * v + (1 - beta2) * (grad ** 2)
-
-    # Step 3: Bias correction
-    # m_hat = m_new / (1 - beta1 ** t)
-    # v_hat = v_new / (1 - beta2 ** t)
-
-    # Step 4: Parameter update
-    # theta_new = theta - alpha * m_hat / (np.sqrt(v_hat) + eps)
-
-    # return theta_new, m_new, v_new
     return (param_new, mt, vt)
